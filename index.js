9527634ec236e310cce0c980b96b23dfe0485929
csulaka <csula.kyeung4@gmail.com>
2018-10-16 04:09:08 +0000

    add existing file

---
+++ index.js
@@ -0,0 +1,69 @@
+
+var express = require('express');
+var socket = require('socket.io');
+
+// app.get('/', (request, response) => {
+//     response.send('Hello World');
+
+// })
+
+// app.get('/json', (request, response) => {
+//     response.send({ hello: 'world'});
+// })
+
+
+//Creating a test Object for javaScirpt // "employees" :
+/*
+var text = '{ "employees" : [' +
+'{ "firstName":"John" , "lastName":"Doe" },' +
+'{ "firstName":"Anna" , "lastName":"Smith" },' +
+'{ "firstName":"Peter" , "lastName":"Jones" } ]}'; 
+*/
+
+
+//monster list
+var text = '{ "monster" : [' +
+'{ "monsterNumber":1, "monsterName":"Godzilla", "attacklocation":"Los Angeles, California", "reporter": "1bc@123.com"},' +
+'{ "monsterNumber":2, "monsterName":"Kaiju and Dracula", "attacklocation":"Tokyo, Japan", "reporter": "jus@bbb.com"},' +
+'{ "monsterNumber":3, "monsterName":"Vampire", "attacklocation":"Boston, Chicago", "reporter": "bch@cdcd.com"} ' +
+']}';
+
+
+
+
+// App setup
+var app = express(); 
+var server = app.listen(4000, function(){
+     console.log('Server is running. On port 4000');
+});
+
+
+//Static files
+app.use(express.static('public'));
+
+//Socket setup
+var io = socket(server);
+
+
+io.on('connection',function(socket){           // Fires callback function
+    console.log('created socket connection')   // each socket will have it's own client that is connected to that server
+
+    //Will emit message to connected users
+    io.sockets.emit('simulated-data', text);    //pass in JSON text
+	
+});
+
+io.on('connection', function(socket) {
+	console.log('created socket connection22222')
+
+	let records = []
+	var mydata = JSON.parse(text);
+	
+	for (x in mydata)
+	{
+		//console.log(mydata[x]);
+		records.push(mydata[x]);			
+	}
+	io.sockets.emit('simulated-data2', records);
+	console.log(records);
+});
---
+++ public/simulate.js
@@ -0,0 +1,43 @@
+
+//Create a connection
+var socket = io.connect('http://localhost:4000');
+
+
+//Query DOM
+var output = document.getElementById('output');
+
+
+socket.on('simulated-data', function  (data){
+    var jsonString = JSON.stringify(JSON.parse(data),null, 4);
+	
+    output.innerHTML += '<pre>' + jsonString + '</pre>';
+
+	
+});
+
+socket.on('simulated-data2', function (data){	
+	for(x in data)
+	{
+		//for( y in data[x]) // print all records
+		{				
+			y = Math.floor(Math.random() * data[x].length); //print random records based on the array's size
+			//var z = Math.floor(Math.random() * y.length;
+			var monsterNum = data[x][y].monsterNumber;
+			var monsterName = data[x][y].monsterName;
+			var location = data[x][y].attacklocation;
+			var numOfVic = Math.floor(Math.random() * 1000);
+			var rep = data[x][y].reporter;
+			
+			count.innerHTML = data[x].length;
+
+			mNum.innerHTML += '<td>' + monsterNum + '</td><br>';
+			mName.innerHTML += '<td>' + monsterName + '</td><br>';
+			loc.innerHTML += '<td>' + location + '</td><br>';
+			reporter.innerHTML += '<td>' + rep + '</td><br>';
+			vic.innerHTML += '<td>' + numOfVic + '</td><br>';
+		}	
+	}
+});
+
+
+
